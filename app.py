fbf3820b49eb146e79475906e24f90dfef0587e4
Fuwenyue <info@fuwenyue.com>
2024-04-15 13:30:14 +0800

    增加 单店单品销售制作

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ import pandas as pd
 import time
 import openpyxl
 from datetime import datetime
+import markdown
 
 #输出文件夹
 folder = 'outputs'
@@ -88,15 +89,35 @@ def index():
         # 将文件名和创建日期添加到列表中
         file_list.append((file, creation_date))
 
-    # 按照创建日期和文件名对文件列表进行降序排序
-    file_list.sort(key=lambda x: (x[1], x[0]), reverse=True)
+    # 按照创建日期降序排序文件列表
+    sorted_file_list = sorted(file_list, key=lambda x: x[1], reverse=True)
+    # 如果文件列表长度大于 10，只保留前 10 个元素
+    if len(sorted_file_list) > 12:
+       sorted_file_list = sorted_file_list[:12]
 
     # 使用排序后的文件列表渲染模板
-    return render_template('index.html', file_list=file_list)
+    return render_template('index.html', file_list=sorted_file_list)
 
 @app.route('/xinpin')
 def xinpin():
     return render_template('xinpin.html')
+
+@app.route('/readme')
+def readme():
+    # 读取 Markdown 文件内容
+    with open('templates/log.md', 'r', encoding='utf-8') as file:
+        markdown_text = file.read()
+
+    # 使用 commonmark 解析 Markdown 文本
+    html_text = markdown.markdown(markdown_text, extensions=['markdown.extensions.tables'])
+
+    # 渲染 HTML 模板并传递 Markdown 转换后的 HTML 内容
+    return render_template('log.html', content=html_text)
+
+
+@app.route('/xiaoshou')
+def xiaoshou():
+    return render_template('xiaoshou.html')
 
 @app.route('/caipin')
 def caipin():
@@ -136,11 +157,6 @@ def xinpin_upload_files():
 @app.route('/download/<filename>')
 def download_file(filename):
     return send_from_directory(folder, filename, as_attachment=True)
-
-@app.route('/download/<filename>')
-def download_output_file(filename):
-    return send_from_directory(directory='Outputs', filename=filename, as_attachment=True)
-
 
 # 处理文件
 def xinpin_process_files(file1_path, file2_path, file3_path=None):
@@ -252,8 +268,8 @@ def caipin_upload_files():
     except:
         caipin_df = pd.read_excel(file1_path)
     duizhao_df = pd.read_excel(file2_path,header=2)
-    duizhao_df = duizhao_df.loc[:,['新增套餐名单品名','标准单品名称','是否为套餐','数量']]
     duizhao_df = duizhao_df[duizhao_df['流程状态_系统字段'] == '已结束']
+    duizhao_df = duizhao_df.loc[:,['新增套餐名单品名','标准单品名称','是否为套餐','数量']]
     result_df = pd.merge(caipin_df, duizhao_df, how='left', left_on='菜品名称',right_on='新增套餐名单品名')
     try:
         result_df['合计数量'] = result_df['销售数量'] *result_df['数量']
@@ -263,6 +279,36 @@ def caipin_upload_files():
     file_link = url_for('static', filename=f'菜品标准名称还原_{now}.xlsx')
     html = f'<a href="{file_link}">下载菜品标准名称还原_{now}.xlsx</a>'
     return html
+
+
+@app.route('/xiaoshou_upload', methods=['POST'])
+def xiaoshou_upload_files():
+    now = time.strftime('%Y%m%d_%H%M', time.localtime())
+    file1 = request.files['file1']
+    file2 = request.files['file2']
+    file1_path = os.path.join('uploads', f'{now}_{file1.filename}')
+    file1.save(file1_path)
+    file2_path = os.path.join('uploads', f'{now}_{file2.filename}')
+    file2.save(file2_path)
+    mendian_df = mendian_format(file1_path)
+    xiaoshou_df,caipin_name = meituan_format(file2_path)
+    df_xiaoshou = pd.merge(mendian_df, xiaoshou_df, how='left', left_on='门店编码',right_on='机构编码').fillna(0)
+    col_name = f'{caipin_name}销售'
+    df_xiaoshou[col_name] =  df_xiaoshou['销售数量'].apply(lambda x: '有销售' if x > 0 else '无销售')
+    output_filename = f'{folder}\\{caipin_name}_单店单品销售统计_{now}.xlsx'
+    df_xiaoshou.to_excel(output_filename, index=False)
+    file_link = url_for('static', filename=f'{caipin_name}_单店单品销售统计_{now}.xlsx')
+    html = f'<a href="{file_link}">下载{caipin_name}_单店单品销售统计_{now}.xlsx</a>'
+    return html
+    
+
+
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
